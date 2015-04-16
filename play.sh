fcd0df5439d589b76f5da2bff5fc651c5ef8e184
robgibbons <robgibbons@users.noreply.github.com>
2015-04-16 01:07:24 -0400

    Create play.sh
    Initial commit

---
+++ play.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Play:
+# A simple video player for Raspberry Pi.
+# Streams media from any local or remote file.
+# Understands YouTube video and playlist URLs!
+
+play () { 
+    local omx="omxplayer -r -g --timeout 30 --threshold 15 -o hdmi"
+    [ -z "$2" ] && $omx "$1" || ([ -z "$3" ] && $omx "$1" -l "$2" || $omx "$1" -l "$2" $3)
+    fbset -depth 8 && fbset -depth 16
+}
+
+# Get location of YouTube files
+yt_file () { 
+    youtube-dl -g "$1"
+}
+
+# Play all videos in YouTube playlist
+yt_plist () { 
+    for url in $(youParse "$1"); do play $(yt_file $url) $2; done 
+}
+
+main () {
+    if [ $# -ne 0 ]; then
+        [[ $1 == *"youtu"* ]] && ( [[ $1 == *"list="* ]] && yt_plist $1 $2 || play $(yt_file $1) $2) || play $1 $2 
+    fi
+}
+
+main $1 $2
+
+exit 0
